d9d1c557b9d006b0dc0969bf75ff6150a6c7f439
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-11 13:00:29 -0500

    created ApplicationUserRepo/Interface

---
+++ WonderDevBlogMVC2024/Data/Repositories/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
+
+namespace WonderDevBlogMVC2024.Data.Repositories
+{
+    public class ApplicationUserRepository : IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public  async Task<IEnumerable<ApplicationUser>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
---
+++ WonderDevBlogMVC2024/Data/Repositories/Interfaces/IApplicationUserRepository.cs
@@ -0,0 +1,10 @@
+﻿// IApplicationUserRepository.cs
+using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories.Interfaces
+{
+    public interface IApplicationUserRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllUsersAsync();
+    }
+}
