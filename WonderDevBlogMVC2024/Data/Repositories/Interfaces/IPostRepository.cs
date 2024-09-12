ccefa505d4a951c45d7fcbf6c1ac47ae1be3d4ce
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-11 19:41:16 -0500

    Created PostRepository & IPostRepository to stay consistant

---
+++ WonderDevBlogMVC2024/Data/Repositories/Interfaces/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetAllPostsAsync();
+        Task<Post> GetPostByIdAsync(int id);
+        Task AddPostAsync(Post post);
+        Task UpdatePostAsync(Post post);
+        Task DeletePostAsync(int id);
+        Task<bool> PostExistsAsync(int id);
+    }
+}
---
+++ WonderDevBlogMVC2024/Data/Repositories/PostRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
+using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddPostAsync(Post post)
+        {
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePostAsync(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Post>> GetAllPostsAsync()
+        {
+            return await _context.Posts
+                .Include(p => p.Author)
+                .Include(p => p.Blog)
+                .ToListAsync();
+        }
+
+
+        public async Task<Post> GetPostByIdAsync(int id)
+        {
+            var post = await _context.Posts
+                .Include(p => p.Author)
+                .Include(p => p.Blog)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (post == null)
+            {
+                throw new KeyNotFoundException($"Post with id {id} not found.");
+            }
+
+            return post;
+        }
+
+        public  async Task<bool> PostExistsAsync(int id)
+        {
+            return await _context.Posts.AnyAsync(p => p.Id == id);
+        }
+
+        public async Task UpdatePostAsync(Post post)
+        {
+            _context.Posts.Update(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
