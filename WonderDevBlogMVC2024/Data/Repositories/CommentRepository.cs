acd13b39a4c2889c1906e9710b5113e8bcbdef18
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-12 17:34:52 -0500

    added repo and repo interface for comments

---
+++ WonderDevBlogMVC2024/Data/Repositories/CommentRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
+using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CommentExistsAsync(int id)
+        {
+            return await _context.Comments.AnyAsync(e => e.Id == id);
+        }
+
+        public  async Task CreateCommentAsync(Comment comment)
+        {
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCommentAsync(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment != null)
+            {
+                _context.Comments.Remove(comment);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllCommentsAsync()
+        {
+            return await _context.Comments
+            .Include(c => c.Author)
+            .Include(c => c.Moderator)
+            .Include(c => c.Post)
+            .ToListAsync();
+        }
+
+        public async Task<Comment?> GetCommentByIdAsync(int id)
+        {
+            return await _context.Comments
+                                 .Include (c => c.Author)
+                                 .Include(c => c.Moderator)
+                                 .Include(c => c.Post)
+                                 .FirstOrDefaultAsync(c=> c.Id == id);
+        }
+
+        public async Task UpdateCommentAsync(Comment comment)
+        {
+            _context.Comments.Update(comment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ WonderDevBlogMVC2024/Data/Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetAllCommentsAsync();
+        Task<Comment?> GetCommentByIdAsync(int id);
+        Task CreateCommentAsync(Comment comment);
+        Task UpdateCommentAsync(Comment comment);
+        Task DeleteCommentAsync(int id);
+        Task<bool> CommentExistsAsync(int id);
+    }
+}
