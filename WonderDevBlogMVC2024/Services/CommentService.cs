bebff6b3bc91d84fc19392a40e0f6eb9d230c41a
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-12 17:37:13 -0500

    added service layer for comments

---
+++ WonderDevBlogMVC2024/Services/CommentService.cs
@@ -0,0 +1,44 @@
+﻿using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
+using WonderDevBlogMVC2024.Models;
+using WonderDevBlogMVC2024.Services.Interfaces;
+
+namespace WonderDevBlogMVC2024.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentService(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllCommentsAsync()
+        {
+            return await _commentRepository.GetAllCommentsAsync();
+        }
+        public async Task<Comment?> GetCommentByIdAsync(int id)
+        {
+            return await _commentRepository.GetCommentByIdAsync(id);
+        }
+        public  async Task CreateCommentAsync(Comment comment)
+        {
+            await _commentRepository.CreateCommentAsync(comment);
+        }
+        public async Task UpdateCommentAsync(Comment comment)
+        {
+            await _commentRepository.UpdateCommentAsync(comment);
+        }
+        public async Task DeleteCommentAsync(int id)
+        {
+             await _commentRepository.DeleteCommentAsync(id);
+        }
+        public async Task<bool> CommentExistsAsync(int id)
+        {
+            return await _commentRepository.CommentExistsAsync(id);
+        }
+
+
+
+    }
+}
