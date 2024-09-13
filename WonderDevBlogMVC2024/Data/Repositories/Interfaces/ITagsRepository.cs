1b6db69c1f3a9b544bc1946043e894df99fea360
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-13 13:13:03 -0500

    created Tag repo layer

---
+++ WonderDevBlogMVC2024/Data/Repositories/Interfaces/ITagsRepository.cs
@@ -0,0 +1,14 @@
+﻿using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories.Interfaces
+{
+    public interface ITagsRepository
+    {
+        Task<IEnumerable<Tag>> GetAllTagsAsync();
+        Task<Tag?> GetTagByIdAsync(int id);
+        Task AddTagAsync(Tag tag);
+        Task UpdateTagAsync(Tag tag);
+        Task DeleteTagAsync(int id);
+        bool TagExists(int id);
+    }
+}
---
+++ WonderDevBlogMVC2024/Data/Repositories/TagsRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
+using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Data.Repositories
+{
+    public class TagsRepository(ApplicationDbContext context) : ITagsRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<IEnumerable<Tag>> GetAllTagsAsync()
+        {
+            return await _context.Tags.Include(t => t.Author)
+                                      .Include(t => t.Post)
+                                      .ToListAsync();
+        }
+
+        public async Task<Tag?> GetTagByIdAsync(int id)
+        {
+            return await _context.Tags.Include(t => t.Author)
+                                      .Include(t => t.Post)
+                                      .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task AddTagAsync(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+        }
+        public  async Task UpdateTagAsync(Tag tag)
+        {
+            _context.Tags.Update(tag);
+            await _context.SaveChangesAsync(); ;
+        }
+
+        public async Task DeleteTagAsync(int id)
+        {
+
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag != null)
+            {
+                _context.Tags.Remove(tag);
+                await _context.SaveChangesAsync();
+            }
+        }
+        
+        public bool TagExists(int id)
+        {
+            return _context.Tags.Any(t => t.Id == id);
+        }
+
+    }
+}
