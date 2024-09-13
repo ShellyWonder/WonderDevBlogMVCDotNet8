f0a86a97a4c712639979b9e6d6bec3c0b8aa7c6d
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-13 13:48:01 -0500

    created Tag service layer

---
+++ WonderDevBlogMVC2024/Services/Interfaces/ITagsService.cs
@@ -0,0 +1,14 @@
+﻿using WonderDevBlogMVC2024.Models;
+
+namespace WonderDevBlogMVC2024.Services.Interfaces
+{
+    public interface ITagsService
+    {
+        Task<IEnumerable<Tag>> GetAllTagsAsync();
+        Task<Tag?> GetTagByIdAsync(int id);
+        Task AddTagAsync(Tag tag);
+        Task UpdateTagAsync(Tag tag);
+        Task DeleteTagAsync(int id);
+        bool TagExists(int id);
+    }
+}
---
+++ WonderDevBlogMVC2024/Services/TagsService.cs
@@ -0,0 +1,40 @@
+﻿using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
+using WonderDevBlogMVC2024.Models;
+using WonderDevBlogMVC2024.Services.Interfaces;
+
+namespace WonderDevBlogMVC2024.Services
+{
+    public class TagsService(ITagsRepository tagsRepository) : ITagsService
+    {
+        private readonly ITagsRepository _tagsRepository = tagsRepository;
+
+        public async Task<IEnumerable<Tag>> GetAllTagsAsync()
+        {
+            return await _tagsRepository.GetAllTagsAsync();
+        }
+
+        public async Task<Tag?> GetTagByIdAsync(int id)
+        {
+            return await _tagsRepository.GetTagByIdAsync(id);
+        }
+        public async Task AddTagAsync(Tag tag)
+        {
+           await _tagsRepository.AddTagAsync(tag);
+        }
+        public async Task UpdateTagAsync(Tag tag)
+        {
+            await _tagsRepository.UpdateTagAsync(tag);
+        }
+        public async Task DeleteTagAsync(int id)
+        {
+            await _tagsRepository.DeleteTagAsync(id);
+        }
+
+        public bool TagExists(int id)
+        {
+            return _tagsRepository.TagExists(id);
+        }
+
+       
+    }
+}
