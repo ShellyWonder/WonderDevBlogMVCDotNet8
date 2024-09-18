1cff0d66d8b1275950f6d2567a86343d7102a7e4
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-17 23:21:31 -0500

    Created Roles enum

---
+++ WonderDevBlogMVC2024/Enums/BlogRole.cs
@@ -0,0 +1,12 @@
+﻿namespace WonderDevBlogMVC2024.Enums
+{
+    public enum BlogRole
+    {
+        Administrator,
+        Moderator,
+        //blog post author permissions
+        Author,
+        //commentor permissions
+        Commentor
+    }
+}
