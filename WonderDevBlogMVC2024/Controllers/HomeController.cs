e2831d59356f6e8e37d9a810e588dfbdbd50156c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-18 16:26:12 -0500

    Implemented the functionality of the "Contact" feature.

--- WonderDevBlogMVC2024/Controllers/HomeController.cs
+++ WonderDevBlogMVC2024/Controllers/HomeController.cs
@@ -1,28 +1,44 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using WonderDevBlogMVC2024.Models;
+using WonderDevBlogMVC2024.Services;
+using WonderDevBlogMVC2024.ViewModels;
 
 namespace WonderDevBlogMVC2024.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<HomeController> _logger = logger;
+        private readonly IBlogEmailSender _emailSender = emailSender;
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+         public IActionResult About()
+        {
+            return View();
+        }
+        //Get
+         public IActionResult Contact()
         {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Contact(ContactUs model)
+        {
+            //build email
+            model.Message = $"{model.Message}<hr>" +
+            //using the Ternary (Conditional) operator (??) to check whether model.Phone is null 
+            $"Phone: {(string.IsNullOrWhiteSpace(model.Phone) ? "Phone: Not Provided" : model.Phone)}";
+            await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
+            return RedirectToAction("Index");
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ WonderDevBlogMVC2024/ViewModels/ContactUs.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WonderDevBlogMVC2024.ViewModels
+{
+    public class ContactUs
+    {
+        public required string Name { get; set; }
+
+        [EmailAddress]
+        [StringLength(80, ErrorMessage = "The {0} must be at least{2} and at most {1} characters long.", MinimumLength = 6)]
+        public required string Email { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+        
+        [StringLength(100, ErrorMessage = "The {0} must be at least{2} and at most {1} characters long.", MinimumLength = 6)]
+        public required string Subject { get; set; }
+
+        [StringLength(750, ErrorMessage = "The {0} must be at least{2} and at most {1} characters long.", MinimumLength = 6)]
+        public required string Message { get; set; }
+    }
+}
