569a67d831a7f6a50a08cbed0ad5072d81e6f248
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-18 23:07:15 -0500

    moved file to a different folder

--- WonderDevBlogMVC2024/Services/Interfaces/IBlogEmailSender.cs
+++ WonderDevBlogMVC2024/Services/Interfaces/IBlogEmailSender.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 
-namespace WonderDevBlogMVC2024.Services
+namespace WonderDevBlogMVC2024.Services.Interfaces
 {
-    public interface IBlogEmailSender :IEmailSender
+    public interface IBlogEmailSender : IEmailSender
     {
         Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage);
     }
