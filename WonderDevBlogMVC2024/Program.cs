a4e79c578dd06b5b1ec8f369d4928483ee827064
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-09-25 11:36:54 -0500

    Implemented Google external login

--- WonderDevBlogMVC2024/Program.cs
+++ WonderDevBlogMVC2024/Program.cs
@@ -7,6 +7,7 @@ using WonderDevBlogMVC2024.Services;
 using WonderDevBlogMVC2024.Services.Interfaces;
 using WonderDevBlogMVC2024.ViewModels;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,9 +18,32 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //Identity configuration w/Roles
-builder.Services.AddIdentity<ApplicationUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders(); 
+   //Use for email confirmation, password reset, etc.
+    .AddDefaultTokenProviders();
+
+// Add Google and GitHub Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    // Ensure the default challenge scheme is set
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme; 
+})
+    .AddGoogle(googleOptions =>
+    {
+        var ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        var ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
+        if (string.IsNullOrEmpty(ClientId) || string.IsNullOrEmpty(ClientSecret))
+        {
+            throw new Exception("Google ClientId or ClientSecret is not set in the configuration.");
+        }
+
+        googleOptions.ClientId = ClientId;
+        googleOptions.ClientSecret = ClientSecret;
+    });
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 //Add Razor components to enhance reusability
@@ -55,7 +79,7 @@ builder.Services.Configure<FormOptions>(options =>
 
 var app = builder.Build();
 
-// Call DataService to set up the database
+// Call DataService to seed the database
 using (var scope = app.Services.CreateScope())
 {
     var dataService = scope.ServiceProvider.GetRequiredService<DataService>();
@@ -66,6 +90,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    builder.Configuration.AddUserSecrets<Program>();
     app.UseMigrationsEndPoint();
 }
 else
@@ -81,7 +106,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
 
 
 // Register MVC route and Razor Pages
--- WonderDevBlogMVC2024/Services/ApplicationUserService.cs
+++ WonderDevBlogMVC2024/Services/ApplicationUserService.cs
@@ -1,30 +1,19 @@
 ﻿using WonderDevBlogMVC2024.Data;
 using WonderDevBlogMVC2024.Data.Repositories.Interfaces;
-using WonderDevBlogMVC2024.Enums;
 using WonderDevBlogMVC2024.Services.Interfaces;
 
 namespace WonderDevBlogMVC2024.Services
 {
-    public class ApplicationUserService(IApplicationUserRepository applicationUserRepository, IImageService imageService) : IApplicationUserService
+    public class ApplicationUserService(IApplicationUserRepository applicationUserRepository) : IApplicationUserService
     {
         private readonly IApplicationUserRepository _applicationUserRepository = applicationUserRepository;
-        private readonly IImageService _imageService = imageService;
+        
 
         public async Task<IEnumerable<ApplicationUser>> GetAllUsersAsync()
         {
             return await _applicationUserRepository.GetAllUsersAsync();
         }
 
-        public  async Task UpdateUserProfileImage(string userId, IFormFile file)
-        {
-            var user = await _applicationUserRepository.GetUserByIdAsync(userId) ?? throw new Exception("User not found");
-            var imageData = await _imageService.ConvertFileToByteArrayAsync(file);
-            var imageType = _imageService.GetFileType(file);
-
-            user.ImageData = imageData;
-            user.ImageType = imageType;
-
-            await _applicationUserRepository.UpdateUserImageAsync(user);
-        }
+        
     }
 }
